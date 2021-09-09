a8c68078b8bf5578117dfcc5db7c58f90b54ebaf
pcvora23 <paragvora19@gmail.com>
2021-09-09 20:07:41 +0530

    REFS #1074 AND FIXES #1074

---
+++ SQL/Parag Vora/Day14/Practice/triggerspractice.sql
@@ -0,0 +1,53 @@
+-- CREATED DATABASE
+    CREATE DATABASE DAY_14_TRIGGERS
+    USE  DAY_14_TRIGGERS
+
+-- CREATING TABLES
+    CREATE TABLE Student
+    (
+        StudentID INT PRIMARY KEY NOT NULL,
+        StudentName VARCHAR(25) NOT NULL,
+        TotalFees SMALLMONEY NOT NULL,
+        RemainingAmt SMALLMONEY NOT NULL
+    )
+
+    CREATE TABLE Course
+    (
+        CourseID INT PRIMARY KEY NOT NULL,
+        CourseName VARCHAR(50) NOT NULL,
+        TotalFees SMALLMONEY NOT NULL
+    )
+
+    CREATE TABLE CourseEnrolled
+    (
+        StudentID INT  CONSTRAINT FK_ST_ID REFERENCES  Student(StudentID),
+        CourseID INT CONSTRAINT FK_COURSE_ID REFERENCES Course(CourseID)
+    )
+
+    CREATE TABLE FeePayment
+    (
+        StudentID INT  CONSTRAINT FK_STUDENT_ID REFERENCES  Student(StudentID),
+        AmountPaid SMALLMONEY NOT NULL,
+        DateofPayment DATE
+    )
+
+
+-- INSERTING DATA
+
+    INSERT INTO Student
+    VALUES
+    (1,'PARAG VORA',15000,7000),
+    (2,'KETAN JAMBUKIYA',12000,3000),
+    (3,'VRAJ MARVANIYA',12000,000),
+    (4,'BHARGAV GANGADHARANI',15000,8000),
+    (5,'JEEL GHODASARA',13500,4000),
+    (6,'BRIJEN SADARIYA',14200,3200),
+    (7,'KARAN SOJITRA',9500,4200),
+    (8,'MILAN SORATHIYA',15000,000),
+    (9,'UMANG VANJA',14200,4200),
+    (10,'RUTVIK BANDHANIYA',13500,3200)
+
+
+
+
+
